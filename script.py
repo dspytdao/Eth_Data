47cc21e5c12a9d8bae8e118ed282988bb4e64ca4
pfed-prog <fedorovn19@gmail.com>
2022-09-27 16:52:33 +0200

    added data

---
+++ script.py
@@ -0,0 +1,24 @@
+import os
+from duneanalytics.duneanalytics import DuneAnalytics
+import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+dune = DuneAnalytics(os.getenv('login'), os.getenv('password'))
+
+# try to login
+dune.login()
+
+# fetch token
+dune.fetch_auth_token()
+
+result_id = dune.query_result_id(query_id=437073)
+
+data = dune.query_result(result_id)
+data = data['data']['get_result_by_result_id']
+
+df = pd.DataFrame(data)
+
+final_df = pd.DataFrame(df.data.tolist(), index= df.index)
+final_df.to_csv('OpenseaPhishing437073.csv')
